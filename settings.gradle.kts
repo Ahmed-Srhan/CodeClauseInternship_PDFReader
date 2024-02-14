ea1497e0e343768ebcc9ffa62e2dcf8b093bc181
Ahmed Srahn <am2831707@gmail.com>
2024-02-14 23:02:31 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+    }
+}
+
+rootProject.name = "CodeClauseInternship_PDFReader"
+include(":app")
+ 
